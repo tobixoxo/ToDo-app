78590989d372e4d0ce747afb9b01a45a81cbf654
tobixoxo <sanjeev.singh.rawat28@gmail.com>
2022-06-19 10:35:51 +0530

    basic frontend with css ready

--- server/routes.py
+++ server/routes.py
@@ -1,12 +1,13 @@
 from server import app
 from server.models import *
 
+from flask import render_template
+
 @app.route('/')
 def index():
-    return "<h1>Hello!</h1>"
+    return render_template('index.html')
 
 @app.route('/dashboard/<int:id>')
 def dashboard(id):
     user_name = User.query.filter(User.id == id).first().first_name
-    print(user_name)
-    return f"<h1>{user_name}</h1>"
+    return render_template('dashboard.html')
